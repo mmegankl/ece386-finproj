0abb9c3fd69bf0e70e79ee21a523518ceb2d6476
mmegankl <c26megan.leong@afacademy.af.edu>
2025-05-01 15:49:58 -0600

    EI w/ Capt Yarbrough 1 May, 70% functional

--- Dockerfile
+++ Dockerfile
@@ -2,20 +2,23 @@
 FROM nvcr.io/nvidia/pytorch:25.02-py3-igpu
 WORKDIR /app
 RUN apt-get update && apt-get install -y --no-install-recommends \
-    # PUT APT LIBRARY HERE \
-    && rm -rf /var/lib/apt/lists/
-RUN apt update && \ 
-    apt install -y --no-install-recommends libportaudiocpp0 libportaudio2 && \
-    apt clean
+    gpiod \
+    libportaudio2 \
+    && rm -rf /var/lib/apt/lists/ && apt clean
+
+COPY requirements.txt .
+
 RUN pip install --upgrade --no-cache-dir pip && \ 
-    pip install --no-cache-dir transformers==4.49.0 accelerate==1.5.2 sounddevice
-COPY speech_recognition.py .
+    pip install --no-cache-dir -r requirements.txt
+    
+    
+COPY ece386fin_proj.py .
 ENV HF_HOME="/huggingface/"
-ENTRYPOINT ["python", "speech_recognition.py"]
-
-
 # In Dockerfile for the GPIO setup
 ENV JETSON_MODEL_NAME=JETSON_ORIN_NANO
 
-# In Dockerfile
-ENV JETSON_MODEL_NAME=JETSON_ORIN_NANO
+ENTRYPOINT ["python", "ece386fin_proj.py"]
+
+
+
+
--- ece386fin_proj.py
+++ ece386fin_proj.py
@@ -1,39 +1,18 @@
-#imports go here
+# imports go here
 import torch
-import sys
-import time
 import sounddevice as sd
 import numpy as np
 import numpy.typing as npt
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, Pipeline, pipeline
 from ollama import chat
-from pydantic import BaseModel
-import sys
 from typing import Optional
+import Jetson.GPIO as GPIO
+import requests
 
-# function declarations
-def initialize():
-    #need to initialize GPIO, mic, models, etc.
 
-    #pin
-    '''Prints 'UP' or 'DOWN' based on edges on Jetson pin #29'''
-    import Jetson.GPIO as GPIO
-
-    # Init as digital input
-    my_pin = 29
-    GPIO.setmode(GPIO.BOARD)  # BOARD pin-numbering scheme
-    GPIO.setup(my_pin, GPIO.IN)  # digital input
-
-    print('Starting Demo! Move pin 29 between 0V and 3.3V')
-
-    while True:
-        GPIO.wait_for_edge(my_pin, GPIO.RISING)
-        print('UP!')
-        GPIO.wait_for_edge(my_pin, GPIO.FALLING)
-        print('down')
-        return 0
-
-def build_whisper_pipeline(model_id: str, torch_dtype: torch.dtype, device: str) -> Pipeline:
+def build_whisper_pipeline(
+    model_id: str, torch_dtype: torch.dtype, device: str
+) -> Pipeline:
     """Creates a Hugging Face automatic-speech-recognition pipeline on the given device."""
     model = AutoModelForSpeechSeq2Seq.from_pretrained(
         model_id,
@@ -54,17 +33,50 @@ def build_whisper_pipeline(model_id: str, torch_dtype: torch.dtype, device: str)
     )
     return pipe
 
-def wait_for_rising_edge():
-    #rising edge for pin 29
-    return 0
 
-def start_recording():
-    #recording from mic
-    return 0
+# 4 second recording
+def record_audio(duration_seconds: int = 4) -> npt.NDArray:
+    """Record duration_seconds of audio from default microphone.
+    Return a single channel numpy array."""
+    sample_rate = 16000  # Hz
+    samples = int(duration_seconds * sample_rate)
+    # Will use default microphone; on Jetson this is likely a USB WebCam
+    audio = sd.rec(samples, samplerate=sample_rate, channels=1, dtype=np.float32)
+    # Blocks until recording complete
+    sd.wait()
+    # Model expects single axis
+    return np.squeeze(audio, axis=1)
 
-def transcribe_audio():
+
+def llm_processor(transcribed_text):
+    weather_input = 0
+    return weather_input
+
+
+def get_weather(weather_input):
+    # call on wttr.in
+    url = "https://wtter.in/{weather_input}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.text
+    else:
+        return None
+
+
+def print_weather(text, weather_input):
+    print(f"The weather in {weather_input} is:")
+
+
+if __name__ == "__main__":
+    # init GPIO
+    # Init as digital input
+    my_pin = 29
+    GPIO.setmode(GPIO.BOARD)  # BOARD pin-numbering scheme
+    GPIO.setup(my_pin, GPIO.IN)  # digital input
+
+    # build Whisper pipeline
     # Get model as argument, default to "distil-whisper/distil-medium.en" if not given
-    model_id = sys.argv[1] if len(sys.argv) > 1 else "distil-whisper/distil-medium.en"
+    model_id = "distil-whisper/distil-medium.en"
     print("Using model_id {model_id}")
     # Use GPU if available
     device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -72,36 +84,21 @@ def transcribe_audio():
     print(f"Using device {device}.")
 
     print("Building model pipeline...")
-    pipe = build_pipeline(model_id, torch_dtype, device)
+    pipe = build_whisper_pipeline(model_id, torch_dtype, device)
     print(type(pipe))
     print("Done")
 
-    print("Recording...")
-    audio = record_audio()
-    print("Done")
+    # wait for GPIO rising edge
+    while True:
+        GPIO.wait_for_edge(my_pin, GPIO.RISING)
+        print("UP!")
 
-    print("Transcribing...")
-    start_time = time.time_ns()
-    speech = pipe(audio)
-    end_time = time.time_ns()
-    print("Done")
+        print("Recording...")
+        audio = record_audio()
+        print("Done")
 
-    print(speech)
-    print(f"Transcription took {(end_time-start_time)/1000000000} seconds")
+        print("Transcribing...")
+        speech = pipe(audio)
+        print("Done")
 
-def llm_processor(transcribed_text):
-    weather_input = 0
-    return weather_input
-
-def get_weather(weather_input):
-    #call on wttr.in
-    url = "https://wtter.in/{weather_input}"
-    weather = requests.get(url)
-    if response.status_coide == 200:
-        return response.text
-    else:
-        return None
-    
-def print_weather(response.text, weather_input):
-    print(f"The weather in {weather_input} is:")
-    
+        print(speech)
